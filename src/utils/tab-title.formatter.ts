dcb3e2ffedcd0166964df3e14748fbf6121756fe
Hapnees <8dwardm8lton@gmail.com>
2023-01-31 19:51:21 +0300

    Edit tab-title.formatter

--- src/utils/tab-title.formatter.ts
+++ src/utils/tab-title.formatter.ts
@@ -1,6 +1,8 @@
 import { Tabs, TabsView } from '@interfaces/tabs.interface'
 
 export const tabTitleFormat = (tab: string) => {
+	if (!tab) return ''
+
 	const array = tab.split('/')
 	const realTabs = Object.keys(Tabs)
 
